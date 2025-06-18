c3aca5cc0a75af6ebcf8dd2a377f3df40d97a3f7
Sarthak-devbits <snai@ciklum.com>
2025-06-18 18:34:22 +0530

    feat: Add duplicate test cases for enhanced coverage in test.js

--- test.js
+++ test.js
@@ -8,4 +8,14 @@ const testCases = [
     datainput:"1 2 3 4 5 \n 6 7 8 9 \n 7 3 4 5 \n 2 2 3 4",
     inputToDisplayOnUI:"[1,2,3,4,5]"
   },
+  {
+    testId: 1,
+    datainput:"1 2 3 4 5 \n 6 7 8 9 \n 7 3 4 5 \n 2 2 3 4",
+    inputToDisplayOnUI:"[1,2,3,4,5]"
+  },
+  {
+    testId: 1,
+    datainput:"1 2 3 4 5 \n 6 7 8 9 \n 7 3 4 5 \n 2 2 3 4",
+    inputToDisplayOnUI:"[1,2,3,4,5]"
+  },
 ];
