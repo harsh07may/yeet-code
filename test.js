a0545d6b5f0713859ff320f3dd8c8479b02f1c07
Sarthak-devbits <snai@ciklum.com>
2025-06-18 18:33:59 +0530

    feat: Add initial test cases and solution structure in test.js

---
+++ test.js
@@ -0,0 +1,11 @@
+const solution = () => {};
+
+const runnerSmallestNumber = () => {};
+
+const testCases = [
+  {
+    testId: 1,
+    datainput:"1 2 3 4 5 \n 6 7 8 9 \n 7 3 4 5 \n 2 2 3 4",
+    inputToDisplayOnUI:"[1,2,3,4,5]"
+  },
+];
